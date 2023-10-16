ed461cf265ffff89ac17e71572452a1a3b900a90
Ashwin Venkatesan <ashwin@platformatory.com>
2023-10-16 21:47:09 +0530

    Modified start.sh and stop.sh script

--- start.sh
+++ start.sh
@@ -5,7 +5,7 @@ echo "Remove any previous instances"
 echo ""
 
 echo "Deploy Kafka Cluster"
-docker compose up -d zookeeper kafka
+docker-compose up -d zookeeper kafka
 echo ""
 
 echo "Wait for Kafka to be in a running state"
@@ -22,11 +22,11 @@ done
 echo ""
 
 echo "Create topics and generate input data"
-docker compose up -d kafka_events
+docker-compose up -d kafka_events
 echo ""
 
 echo "Deploy Flink Cluster"
-docker compose up -d jobmanager taskmanager
+docker-compose up -d jobmanager taskmanager
 sleep 10
 while true; do
     if nc -z -w 1 localhost 8081; then
@@ -40,21 +40,21 @@ done
 echo ""
 
 echo "Create Flink Stream Processing Job"
-docker compose up -d jobrunner
+docker-compose up -d jobrunner
 echo ""
 
 echo "Deploy Redis and Postgres for online Store and Feature registry"
-docker compose up -d postgres redis
+docker-compose up -d postgres redis
 echo ""
 
 echo "Deploy Feast server"
-docker compose up -d feastserver
+docker-compose up -d feastserver
 echo ""
 
 sleep 10
 
 echo "Ingest features from Kafka into Feast"
-docker compose up -d feastingest
+docker-compose up -d feastingest
 
 echo "Deploy Model Serving Layer"
-docker compose up -d modelapi
+docker-compose up -d modelapi
